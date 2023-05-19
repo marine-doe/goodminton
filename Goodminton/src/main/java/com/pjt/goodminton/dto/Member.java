ccc84257a74042396d6abd3fac3def8c03815a74
Kim <nayeon0016@gmail.com>
2023-05-19 16:30:53 +0900

    dto최종

---
+++ Goodminton/src/main/java/com/pjt/goodminton/dto/Member.java
@@ -0,0 +1,45 @@
+package com.pjt.goodminton.dto;
+
+public class Member {
+	private int id;
+	private int bookId;
+	private int userId;
+	
+	public Member() {
+	}
+	
+	public Member(int id, int bookId, int userId) {
+		this.id = id;
+		this.bookId = bookId;
+		this.userId = userId;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", bookId=" + bookId + ", userId=" + userId + "]";
+	}
+}
